d7303ee6abed8486db8cb1cdac47fed9ef5d9f56
imsaurabh17 <saurabhdubey222786@gmail.com>
2026-03-17 18:58:49 +0530

    add optimized code for binary search with O(log n) time complexity

--- practice_questions/binary_search.py
+++ practice_questions/binary_search.py
@@ -1,15 +1,23 @@
 def binary_search(arr: list, target: int) -> int:
     """This function will output the index of a target number from a given array"""
 
-    for index, num in enumerate(arr):
-        if num == target:
-            return index
+    left, right = 0, len(arr) - 1
+
+    while left <= right:
+
+        mid = (left + right) // 2
+
+        if arr[mid] == target:
+            return mid
         
-    else:
-        return f"Target isn't in the provided array"
+        elif arr[mid] <= target:
+            left = mid + 1
+
+        else:
+            right = mid - 1
     
 
 if __name__=="__main__":
     arr = [1,2,3,4,5,6,7,8]
-    target = 10
+    target = 5
     print(binary_search(arr,target))
